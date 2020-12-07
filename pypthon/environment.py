44ba9fc2e4f28c0a4b8cb6c172a0b7d095d03a75
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-07 10:33:39 -0800

    added uprint to env

--- pypthon/environment.py
+++ pypthon/environment.py
@@ -4,3 +4,14 @@ import sys
 from pathlib import Path
 from pprint import pprint
 from sys import stdin
+
+def uprint(kwargs, iterable):
+    """
+    Allows unpacking into print
+
+    Usage:
+
+    >>> ["hello", "world"] | uprint {"sep": ":"}
+    hello:world
+    """
+    print(*iterable, **kwargs)
