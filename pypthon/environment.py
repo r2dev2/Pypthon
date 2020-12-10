8063205485e4c85515861f7342565063687172f5
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-10 00:20:40 -0800

    added inline shell function

--- pypthon/environment.py
+++ pypthon/environment.py
@@ -1,7 +1,9 @@
 import itertools as it
 import os
 import re
-import shutil as sh
+import shlex
+import shutil
+import subprocess
 import sys
 from functools import reduce
 from pathlib import Path
@@ -31,3 +33,18 @@ def ufunc(func, iterable):
     hello world
     """
     return func(*iterable)
+
+
+def sh(command, iterable):
+    """
+    Opens a shell function.
+
+    This streams input but not the response.
+    """
+    pipe = subprocess.PIPE
+    process = subprocess.Popen(shlex.split(command), universal_newlines=True, stdin=pipe, stdout=pipe, stderr=pipe)
+    for line in iterable:
+        process.stdin.write(line + '\n')
+        process.stdin.flush()
+    process.stdin.close()
+    return process.stdout
