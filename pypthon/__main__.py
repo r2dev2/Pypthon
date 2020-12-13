0d3d47a7f5ffd81beacce0f687a1b1beda1ead47
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-12 21:39:36 -0800

    preliminary support for importing modules

--- pypthon/__main__.py
+++ pypthon/__main__.py
@@ -14,12 +14,23 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Python with pipes")
     parser.add_argument("command", help="the pypthon command")
     parser.add_argument("-s", "--show-python", help="show python code generated", action="store_true")
+    parser.add_argument("-i", "--import", help="module to import eg: -i 'requests' -i 'numpy'", default=[], action="append")
     args = parser.parse_args()
     python = parse_command(args.command)
     if args.show_python:
         print(python)
-    exec(compile(get_user_custom_setup(), "<string>", "exec"))
-    eval(python)
+    code = '\n'.join([
+        '\n'.join("import %s" % mod for mod in getattr(args, "import")),
+        get_user_custom_setup(),
+        python
+    ])
+    print(code)
+    exec(compile(code, "<string>", "exec"))
+    # for module in getattr(args, "import"):
+    #     import_string = f"import {module}"
+    #     exec(compile(import_string, "<string", "exec"))
+    # exec(compile(get_user_custom_setup(), "<string>", "exec"))
+    # eval(python)
 
 
 def get_user_custom_setup() -> str:
