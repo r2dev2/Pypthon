e3013185dc4bd9b44fc1468c1ca89ca18fc0a9f3
r2dev2bb8 <ronak.badhe@gmail.com>
2020-12-14 15:31:08 -0800

    added environment and dry run mode

--- pypthon/__main__.py
+++ pypthon/__main__.py
@@ -15,9 +15,11 @@ def main() -> None:
     parser.add_argument("command", help="the pypthon command")
     parser.add_argument("-s", "--show-python", help="show python code generated", action="store_true")
     parser.add_argument("-i", "--import", help="module to import eg: -i 'requests' -i 'numpy'", default=[], action="append")
+    parser.add_argument("-d", "--dry-run", help="don't run python code", action="store_true")
+    parser.add_argument("-e", "--environment", help="show variables excluding -i imports in environment", action="store_true")
     args = parser.parse_args()
     python = parse_command(args.command)
-    if args.show_python:
+    if args.show_python or args.dry_run:
         print(python)
     code = '\n'.join([
         get_user_custom_setup(),
@@ -25,8 +27,12 @@ def main() -> None:
     ])
     for module in getattr(args, "import"):
         import_string = f"import {module}"
-        exec(import_string, globals())
-    exec(code)
+        if not args.dry_run:
+            exec(import_string, globals())
+    if args.dry_run or args.environment:
+        pprint(sorted(v for v in globals() if v[:1] != "_"))
+    if not args.dry_run:
+        exec(code)
 
 
 def get_user_custom_setup() -> str:
